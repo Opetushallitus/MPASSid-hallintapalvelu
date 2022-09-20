fba40c034cf1171aead445217343de6d91a19704
Arno Saine <arno@mowhi.com>
2022-09-20 14:30:45 +0300

    Piilota teeman kehityksen apukomponenttien linkki tuotantoversiosta

--- mpassid-voh-ui/src/layouts/Basic.tsx
+++ mpassid-voh-ui/src/layouts/Basic.tsx
@@ -1,20 +1,36 @@
 import useSetDocumentTitle from "@/hooks/useDocumentTitle";
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Fab, Typography } from "@mui/material";
 import { FormattedMessage } from "react-intl";
 import { Link, Outlet } from "react-router-dom";
+import StyleIcon from "@mui/icons-material/Style";
 
 export default function Basic() {
   useSetDocumentTitle();
 
   return (
-    <Container>
-      <Box p={3}>
-        <Typography variant="h1" gutterBottom>
-          <FormattedMessage defaultMessage="MPASSid-hallinta" />
-        </Typography>
-        <Outlet />
-        <Link to="theme-development-helpers">Theme development helpers</Link>
-      </Box>
-    </Container>
+    <>
+      <Container>
+        <Box p={3}>
+          <Typography variant="h1" gutterBottom>
+            <FormattedMessage defaultMessage="MPASSid-hallinta" />
+          </Typography>
+          <Outlet />
+        </Box>
+      </Container>
+      {!ENV.PROD && (
+        <Fab
+          size="small"
+          component={Link}
+          to="theme-development-helpers"
+          sx={{
+            position: "absolute",
+            bottom: 16,
+            right: 16,
+          }}
+        >
+          <StyleIcon />
+        </Fab>
+      )}
+    </>
   );
 }
