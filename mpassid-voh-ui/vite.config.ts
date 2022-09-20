d8289eda46bc2ac819132abef4014859d60bafb3
Arno Saine <arno@mowhi.com>
2022-09-20 14:48:12 +0300

    TS import patch

--- mpassid-voh-ui/vite.config.ts
+++ mpassid-voh-ui/vite.config.ts
@@ -6,6 +6,15 @@ export default defineConfig({
   base:
     (process.env.NODE_ENV === "production" && process.env.VITE_BASENAME) ||
     undefined,
+  resolve: {
+    // https://github.com/vitejs/vite/issues/3040
+    alias:
+      process.env.NODE_ENV !== "production"
+        ? {
+            "./useLocale.js": "./useLocale.ts",
+          }
+        : undefined,
+  },
   plugins: [
     superTemplate({
       reactIntlBundledMessages: { noParser: false },
