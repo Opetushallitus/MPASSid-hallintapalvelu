08e74b92b62e1f8830d8baaa732cfe5fe7a9a128
jani.heikkinen <jani.heikkinen@csc.fi>
2023-01-25 11:31:03 +0200

    Configuration for UserDetailsService

---
+++ mpassid-voh-api/src/main/java/fi/mpass/voh/api/config/OPHUserDetailsServiceConfig.java
@@ -0,0 +1,67 @@
+package fi.mpass.voh.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+
+import fi.vm.sade.javautils.kayttooikeusclient.OphUserDetailsServiceImpl;
+
+import java.util.List;
+
+@Profile("!dev")
+@Configuration
+@PropertySource("classpath:kayttooikeus.properties")
+public class OPHUserDetailsServiceConfig {
+    private static final SimpleGrantedAuthority[] RESTRICTED_AUTHORITIES = new SimpleGrantedAuthority[] {
+            new SimpleGrantedAuthority("ROLE_APP_MPASSID")
+    };
+
+    @Value("${kayttooikeus.server.prefix}")
+    private String kayttooikeusServerPrefix;
+
+    @Value("${kayttooikeus.client.callerid}")
+    private String kayttooikeusClientCallerId;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetailsService userDetails;
+        try {
+            userDetails = new OphUserDetailsServiceImpl(kayttooikeusServerPrefix, kayttooikeusClientCallerId);
+        } catch (Exception e) {
+            System.out.println("Exception through OphUserDetailsServiceImpl: " + e);
+            userDetails = new OPHUserDetailsService(passwordEncoder());
+        }
+        return userDetails;
+    }
+
+    static class OPHUserDetailsService implements UserDetailsService {
+        PasswordEncoder passwordEncoder;
+
+        private OPHUserDetailsService(PasswordEncoder passwordEncoder) {
+            this.passwordEncoder = passwordEncoder;
+        }
+
+        @Override
+        public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+            switch (username) {
+                default:
+                    return User.builder()
+                            .authorities(List.of(RESTRICTED_AUTHORITIES))
+                            .password(this.passwordEncoder.encode("t9qeub211o"))
+                            .username(username)
+                            .build();
+            }
+        }
+    }
+}
