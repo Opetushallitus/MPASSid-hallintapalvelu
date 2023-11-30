4db11a6bf9f299f806ded095e2ff0c1175aaea6c
jani.heikkinen <jani.heikkinen@csc.fi>
2023-11-30 11:32:48 +0200

    modified PermissionEvaluator to accept and verify the organization of the target integration againts the authenticated authority organization information

--- mpassid-voh-api/src/main/java/fi/mpass/voh/api/integration/OPHPermissionEvaluator.java
+++ mpassid-voh-api/src/main/java/fi/mpass/voh/api/integration/OPHPermissionEvaluator.java
@@ -1,16 +1,26 @@
 package fi.mpass.voh.api.integration;
 
 import java.io.Serializable;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class OPHPermissionEvaluator implements PermissionEvaluator {
     private final static Logger logger = LoggerFactory.getLogger(OPHPermissionEvaluator.class);
+
+    @Value("${application.admin-organization-oid}")
+    private String adminOrganizationOid;
+
+    private final IntegrationRepository integrationRepository;
+
+    public OPHPermissionEvaluator(IntegrationRepository integrationRepository) {
+        this.integrationRepository = integrationRepository;
+    }
 
     /**
      * The method asserts that the authentication object includes the required
@@ -61,14 +71,86 @@ public class OPHPermissionEvaluator implements PermissionEvaluator {
         return false;
     }
 
+    /**
+     * The method asserts that the authentication object includes the required
+     * authority. The method trusts the authorization-service to return
+     * valid authority information.
+     * 
+     * A set of example authority roles returned by the authorization-service:
+     * 1 ROLE_APP_MPASSID
+     * 2 ROLE_APP_MPASSID_TALLENTAJA
+     * 3 ROLE_APP_MPASSID_TALLENTAJA_1.2.246.562.10.90008375488
+     * 4 ROLE_APP_MPASSID_KATSELIJA
+     * 5 ROLE_APP_MPASSID_KATSELIJA_1.2.246.562.10.90008375488
+     * 
+     * @param auth               authentication object
+     * @param id                 the id of the target integration to which
+     *                           authorization is requested
+     * @param requiredPermission the required privilege
+     * @return boolean true when the authentication object includes the required
+     *         permission in valid format and the target integration organization
+     *         oid matches the granted permission organization oid, otherwise false
+     */
+    private boolean hasPrivilege(Authentication auth, Long id, String requiredPermission) {
+
+        Optional<Integration> existingIntegration = this.integrationRepository.findById(id);
+
+        if (existingIntegration.isPresent()) {
+            logger.debug("target integration #" + existingIntegration.get().getId());
+            logger.debug("number of granted authorities: " + auth.getAuthorities().size());
+            for (GrantedAuthority grantedAuth : auth.getAuthorities()) {
+                logger.info("granted authority: " + grantedAuth.getAuthority());
+
+                if (grantedAuth.getAuthority().length() > 0) {
+                    String[] authorityElements = (grantedAuth.getAuthority()).split("_");
+
+                    // (1) The granted authority must include a permission element when there is a
+                    // required permission
+                    if (requiredPermission.length() > 0 && authorityElements.length < 4) {
+                        continue;
+                    }
+                    if (authorityElements.length > 2) {
+                        // (1, 2, 3, 4, 5) The granted authority must include the target application
+                        // identifier in the 3rd element
+                        if (authorityElements[2].contains("MPASSID")) {
+                            if (requiredPermission.length() == 0)
+                                return true;
+                            if (authorityElements.length > 3) {
+                                // (2, 3, 4, 5) The granted authority must include the required permission
+                                if (authorityElements[3].contains(requiredPermission)) {
+                                    if (authorityElements.length > 4) {
+                                        // (3, 5) the granted authority must include the integration organization oid
+                                        if (authorityElements[4]
+                                                .equals(existingIntegration.get().getOrganization().getOid())
+                                                || authorityElements[4].equals(this.adminOrganizationOid)) {
+                                            return true;
+                                        }
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
     @Override
     public boolean hasPermission(Authentication auth, Object targetDomainObject, Object permission) {
         if ((auth == null) || (targetDomainObject == null) || !(permission instanceof String)) {
             return false;
         }
-        String targetType = targetDomainObject.getClass().getSimpleName().toUpperCase();
 
-        return hasPrivilege(auth, targetType, permission.toString().toUpperCase());
+        if (targetDomainObject instanceof Long) {
+            return hasPrivilege(auth, (Long) targetDomainObject, permission.toString().toUpperCase());
+        }
+        if (targetDomainObject instanceof String) {
+            String targetType = targetDomainObject.getClass().getSimpleName().toUpperCase();
+            return hasPrivilege(auth, targetType, permission.toString().toUpperCase());
+        }
+        return false;
     }
 
     @Override
