f3391bdbc754fedb7dbc07acce1e6da9e7572292
jani.heikkinen <jani.heikkinen@csc.fi>
2023-12-07 14:14:19 +0200

    added implementation and tests for loading persistent integration set data

--- mpassid-voh-api/src/main/java/fi/mpass/voh/api/config/IntegrationSetLoader.java
+++ mpassid-voh-api/src/main/java/fi/mpass/voh/api/config/IntegrationSetLoader.java
@@ -5,6 +5,8 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 import com.fasterxml.jackson.core.json.JsonReadFeature;
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -23,11 +25,15 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
 
 import fi.mpass.voh.api.integration.Integration;
+import fi.mpass.voh.api.integration.IntegrationDiffBuilder;
 import fi.mpass.voh.api.integration.IntegrationRepository;
+import fi.mpass.voh.api.integration.attribute.Attribute;
 import fi.mpass.voh.api.integration.set.IntegrationSet;
 import fi.mpass.voh.api.organization.Organization;
 import fi.mpass.voh.api.organization.OrganizationService;
 
+import org.apache.commons.lang3.builder.Diff;
+import org.apache.commons.lang3.builder.DiffResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -66,6 +72,9 @@ public class IntegrationSetLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        List<Long> setIds = integrationRepository.getAllSetIds();
+        logger.debug("Number of existing integration sets: " + setIds.size());
+
         for (String setInput : this.integrationSetInput) {
             ObjectMapper objectMapper = new ObjectMapper();
             objectMapper.configure(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS.mappedFeature(), true);
@@ -103,6 +112,89 @@ public class IntegrationSetLoader implements CommandLineRunner {
                         logger.error(
                                 "Integration set exception: " + e + " continuing to next.");
                         continue;
+                    }
+
+                    // an existing integration
+                    if (setIds.contains(integrationSet.getId())) {
+                        Optional<Integration> existingIntegration = this.integrationRepository
+                                .findByIdAll(integrationSet.getId());
+
+                        if (existingIntegration.isPresent()) {
+                            logger.debug("Comparing existing integration " + existingIntegration.get().getId()
+                                    + " version " + existingIntegration.get().getVersion() + " to "
+                                    + integrationSet.getId() + " version " + integrationSet.getVersion());
+                       
+                            DiffResult<Integration> diff = IntegrationDiffBuilder.compareSet(existingIntegration.get(),
+                                    integrationSet);
+
+                            if (diff != null) {
+                                List<Diff<?>> diffs = diff.getDiffs();
+                                for (int i = 0; i < diff.getNumberOfDiffs(); i++) {
+                                    Diff<?> d = diffs.get(i);
+                                    logger.debug(d.getFieldName() + ": " + d.getLeft() + " != " + d.getRight());
+                                    String[] diffElements = d.getFieldName().split("\\.");
+                                    if (d.getFieldName().contains("configurationEntity.attributes.")) {
+
+                                        // existing = left = "", input = right != ""
+                                        // 1. a new attribute (with a new value) has been added to the integration
+                                        // context
+                                        if (d.getLeft().equals("") && !d.getRight().equals("")) {
+                                            logger.debug("Add diff: " + d.getFieldName());
+                                            Set<Attribute> existingAttributes = existingIntegration.get()
+                                                    .getConfigurationEntity().getAttributes();
+                                            // TODO verify the i+1 and i+2 fieldNames
+                                            // type, name, content
+                                            Attribute newAttr = new Attribute(diffs.get(i + 1).getRight().toString(),
+                                                    diffs.get(i).getRight().toString(),
+                                                    diffs.get(i + 2).getRight().toString());
+                                            existingAttributes.add(newAttr);
+                                            i = i + 2;
+                                            continue;
+                                        }
+                                        // (existing = left) != (input = right)
+                                        // 2. the value has been changed
+                                        if (!d.getLeft().equals("") && !d.getRight().equals("")
+                                                && !d.getLeft().equals(d.getRight())) {
+                                            // get the attr from the set by name (id exists?)
+                                            for (Attribute a : existingIntegration.get().getConfigurationEntity()
+                                                    .getAttributes()) {
+                                                // attribute name
+                                                if (diffElements.length>1 && a.getName().equals(diffElements[2])) {
+                                                    // TODO verify diff fieldName matches attribute name -> attribute
+                                                    // content
+                                                    // update existing integration attribute content with input
+                                                    a.setContent(d.getRight().toString());
+                                                    // remove from set, add to set
+                                                    existingIntegration.get().getConfigurationEntity().getAttributes()
+                                                            .remove(a);
+                                                    existingIntegration.get().getConfigurationEntity().getAttributes()
+                                                            .add(a);
+                                                }
+                                            }
+
+                                            i = i + 2;
+                                            continue;
+                                        }
+                                        // existing = left != "", input = right == ""
+                                        // 3. the existing attribute has been removed from the input in the integration
+                                        // context
+                                        if (!d.getLeft().equals("") && d.getRight().equals("")) {
+                                            for (Attribute a : existingIntegration.get().getConfigurationEntity()
+                                                    .getAttributes()) {
+                                                // attribute name
+                                                if (a.getName().equals(diffElements[2])) {
+                                                    existingIntegration.get().getConfigurationEntity().getAttributes()
+                                                            .remove(a);
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            } else {
+                                logger.debug("Comparison failed. Check input data structure and values.");
+                            }
+                        }
+                        setIds.remove(integrationSet.getId());
                     }
 
                     Organization organization = new Organization();
