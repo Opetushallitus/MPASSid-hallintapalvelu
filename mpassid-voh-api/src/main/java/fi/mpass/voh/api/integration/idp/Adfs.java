aebe86fc79bb8946531d8048a3cfa40c9fecddc8
jani.heikkinen <jani.heikkinen@csc.fi>
2024-03-18 13:53:27 +0200

    migrated from javax to jakarta, relocated identity provider specific fields (identifiers), added missing pre-authorization

--- mpassid-voh-api/src/main/java/fi/mpass/voh/api/integration/idp/Adfs.java
+++ mpassid-voh-api/src/main/java/fi/mpass/voh/api/integration/idp/Adfs.java
@@ -2,15 +2,14 @@ package fi.mpass.voh.api.integration.idp;
 
 import java.time.LocalDate;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
 
 import org.hibernate.envers.Audited;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import fi.mpass.voh.api.integration.mp.SamlMetadataProvider;
-import io.swagger.v3.oas.annotations.media.Schema;
 
 @Audited
 @Entity
@@ -18,8 +17,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Adfs extends IdentityProvider {
 
-    @Schema(example = "https://example.org/6ab309b7-f4d4-455a-9c88-857474ceea33")
-    private String entityId;
+    //@Schema(example = "https://example.org/6ab309b7-f4d4-455a-9c88-857474ceea33")
+    //private String entityId;
 
     private String metadataUrl;
 
@@ -29,7 +28,7 @@ public class Adfs extends IdentityProvider {
 
     public Adfs() {
         super();
-    };
+    }
 
     public Adfs(String idpId, String logoUrl, String flowName) {
         super(idpId, logoUrl, flowName);
