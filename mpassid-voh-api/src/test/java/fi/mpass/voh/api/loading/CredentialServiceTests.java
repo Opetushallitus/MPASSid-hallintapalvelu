0be7dfd92c08c71f39002581e8bf58a95f6129b8
jani.heikkinen <jani.heikkinen@csc.fi>
2024-06-07 09:49:51 +0300

    added tests for credential updates

--- mpassid-voh-api/src/test/java/fi/mpass/voh/api/loading/CredentialServiceTests.java
+++ mpassid-voh-api/src/test/java/fi/mpass/voh/api/loading/CredentialServiceTests.java
@@ -1,27 +1,82 @@
 package fi.mpass.voh.api.loading;
 
-import org.junit.jupiter.api.Disabled;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled
+import static org.mockito.BDDMockito.given;
+
+import fi.mpass.voh.api.integration.ConfigurationEntity;
+import fi.mpass.voh.api.integration.DiscoveryInformation;
+import fi.mpass.voh.api.integration.Integration;
+import fi.mpass.voh.api.integration.idp.Opinsys;
+import fi.mpass.voh.api.organization.Organization;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class CredentialServiceTests {
-    @Test
-    void testFinish() {
 
+    @Mock
+    private ParameterStoreService parameterStoreService;
+
+    private CredentialService underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new CredentialService(parameterStoreService);
     }
 
     @Test
-    void testStart() {
+    void testUpdateCredential() {
 
+        DiscoveryInformation discoveryInformation = new DiscoveryInformation("Custom Display Name",
+                "Custom Title", true);
+        Organization organization = new Organization("Organization zyx", "1.2.3.4.5.6.7.8");
+        ConfigurationEntity configurationEntity = new ConfigurationEntity();
+        Opinsys opinsys = new Opinsys("tenantId");
+        configurationEntity.setIdp(opinsys);
+        Integration integration = new Integration(99999L, LocalDate.now(), configurationEntity, LocalDate.of(2023, 6, 30),
+                0, discoveryInformation, organization,
+                "serviceContactAddress@example.net");
+
+        // given
+        given(parameterStoreService.put(any(String.class), any(String.class), any(String.class))).willReturn(true);
+
+        // when
+        boolean success = underTest.updateCredential(integration, "client_id", "5432§1assdfg");
+
+        // then
+        assertTrue(success);
     }
 
     @Test
-    void testUpdateCredentialName() {
+    void testUpdateCredentialWithFailingParameterStoreService() {
 
-    }
+        DiscoveryInformation discoveryInformation = new DiscoveryInformation("Custom Display Name",
+                "Custom Title", true);
+        Organization organization = new Organization("Organization zyx", "1.2.3.4.5.6.7.8");
+        ConfigurationEntity configurationEntity = new ConfigurationEntity();
+        Opinsys opinsys = new Opinsys("tenantId");
+        configurationEntity.setIdp(opinsys);
+        Integration integration = new Integration(99999L, LocalDate.now(), configurationEntity, LocalDate.of(2023, 6, 30),
+                0, discoveryInformation, organization,
+                "serviceContactAddress@example.net");
 
-    @Test
-    void testUpdateCredentialValue() {
+        // given
+        given(parameterStoreService.put(any(String.class), any(String.class), any(String.class))).willReturn(false);
 
+        // when
+        boolean success = underTest.updateCredential(integration, "client_id", "5432§1assdfg");
+
+        // then
+        assertFalse(success);
     }
 }
