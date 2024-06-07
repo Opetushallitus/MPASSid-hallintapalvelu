e3045d1932cecf0c16d6764a9c8d6b2a2b91da09
jani.heikkinen <jani.heikkinen@csc.fi>
2024-06-07 09:41:44 +0300

    configurable parameter store parameters

--- mpassid-voh-api/src/main/java/fi/mpass/voh/api/loading/ParameterStoreService.java
+++ mpassid-voh-api/src/main/java/fi/mpass/voh/api/loading/ParameterStoreService.java
@@ -2,57 +2,60 @@ package fi.mpass.voh.api.loading;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ssm.SsmClient;
 import software.amazon.awssdk.services.ssm.model.*;
 
+@Service
 public class ParameterStoreService {
     private static final Logger logger = LoggerFactory.getLogger(ParameterStoreService.class);
 
-    // TODO configurable
-    private static final Region region = Region.EU_NORTH_1;
-    private static final String KMS_KEY_ID = "alias/mpassid-poc-key";
+    private static final String DELIM = "/";
+
+    @Value("${application.parameterstore.region:eu-north-1}")
+    private String parameterStoreRegion = "eu-north-1";
+
+    @Value("${application.parameterstore.kms-key-id:mpassid-poc-key}")
+    private String kmsKeyId = "alias/mpassid-poc-key";
+    
+    @Value("${application.parameterstore.parameterRootName:mpassid}")
+    private String parameterRootName = "/MPASSID/organization";
+
     private final SsmClient ssmClient;
-    private String parameterName = "/MPASSID/organization";
 
-    public ParameterStoreService(SsmClient ssmClient) {
-        this.ssmClient = ssmClient;
-    }
-
-    /**
-     * Creates a new ParameterStoreService for the profile
-     *
-     * @return A new ParameterStoreService for the profile
-     */
-    public static ParameterStoreService build() {
-        SsmClient ssmClient = SsmClient.builder()
+    public ParameterStoreService() {
+        Region region = Region.of(parameterStoreRegion);
+        ssmClient = SsmClient.builder()
                 .credentialsProvider(ProfileCredentialsProvider.create("mpassid-rr-dev"))
                 .region(region)
                 .build();
-        return new ParameterStoreService(ssmClient);
     }
 
     public boolean put(String id, String name, String value) {
-        // TODO
-        String path = parameterName + "/" + id + "/" + name;
+        String path = parameterRootName + "/" + id + "/" + name;
 
         PutParameterRequest request = PutParameterRequest
                 .builder()
                 .name(path)
                 .overwrite(true) // allow insert or update semantics
                 .type(ParameterType.SECURE_STRING)
-                .keyId(KMS_KEY_ID)
+                .keyId(kmsKeyId)
                 .value(value)
                 .build();
         try {
             PutParameterResponse res = ssmClient.putParameter(request);
             logger.debug("Result: {}", res);
+            logger.debug("HTTP result: {}", res.sdkHttpResponse());
         } catch (Exception e) {
             logger.error("Exception: ", e);
+            value = null;
             return false;
         }
+        value = null;
         return true;
     }
 
