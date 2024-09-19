f2c7227469d7f02520738407ff5aa7a2b2a9ac8a
Jarkko Toivonen <jarkko.toivonen@csc.fi>
2024-09-19 10:04:12 +0300

    Disable development logs from production

--- mpassid-voh-ui/src/utils/devLog.tsx
+++ mpassid-voh-ui/src/utils/devLog.tsx
@@ -1,7 +1,5 @@
 export const devLog = (text:string, data:any) => {
     if(!ENV.PROD) {
       console.log("DEBUG: ",text,": ",data)
-    } else {
-      console.log("DEBUG: ",text,": ",data)
-    }
+    } 
   }
