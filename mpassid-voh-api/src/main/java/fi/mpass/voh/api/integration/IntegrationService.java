4e88fd13bc14e5ce7594cebe10f3a2bcf99a85c2
Eero Rantala <eero.rantala@csc.fi>
2025-06-03 16:50:51 +0300

    id check added to changeMetadataUrlForProvisioning()

--- mpassid-voh-api/src/main/java/fi/mpass/voh/api/integration/IntegrationService.java
+++ mpassid-voh-api/src/main/java/fi/mpass/voh/api/integration/IntegrationService.java
@@ -1274,14 +1274,14 @@ public class IntegrationService {
     if (integration.getConfigurationEntity().getIdp() instanceof IdentityProvider) {
       if (integration.getConfigurationEntity().getIdp().getType().equals("adfs")) {
         Adfs adfsIdp = (Adfs) integration.getConfigurationEntity().getIdp();
-        if (adfsIdp.getMetadataUrl().contains(configuration.getMetadataBaseUrlUi())) {
+        if (adfsIdp.getMetadataUrl().contains(configuration.getMetadataBaseUrlUi()) && adfsIdp.getMetadataUrl().endsWith(integration.getId().toString())) {
           String metadataUrl = this.configuration.getMetadataBaseUrl()
               + "/" + integration.getId();
           adfsIdp.setMetadataUrl(metadataUrl);
         }
       } else if (integration.getConfigurationEntity().getIdp().getType().equals("gsuite")) {
         Gsuite gsuiteIdp = (Gsuite) integration.getConfigurationEntity().getIdp();
-        if (gsuiteIdp.getMetadataUrl().contains(configuration.getMetadataBaseUrlUi())) {
+        if (gsuiteIdp.getMetadataUrl().contains(configuration.getMetadataBaseUrlUi()) && gsuiteIdp.getMetadataUrl().endsWith(integration.getId().toString())) {
           String metadataUrl = this.configuration.getMetadataBaseUrl()
               + "/" + integration.getId();
           gsuiteIdp.setMetadataUrl(metadataUrl);
